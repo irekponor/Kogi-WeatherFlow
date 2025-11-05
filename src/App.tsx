e34174ea062b220965c6e122de4a7cb21f01500c
unknown <emmanuelirekponor86@gmail.com>
2025-11-05 01:30:30 +0100

    geojson

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
- 
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
 
-function App() {
-   
+const App = () => {
+  const [geoData, setGeoData] = useState<any>(null);
+
+  useEffect(() => {
+    fetch("/Lagoslga.geojson")
+      .then((res) => res.json())
+      .then((data) => setGeoData(data))
+      .catch((err) => console.error("Error loading GeoJSON:", err));
+  }, []);
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">hello</h1>
-          </>
-  )
-}
+    <div className="h-screen w-screen">
+      <MapContainer
+        center={[6.5244, 3.3792]}
+        zoom={10}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {geoData && (
+          <GeoJSON
+            data={geoData}
+            style={{
+              color: "#1E90FF",
+              weight: 2,
+              fillOpacity: 0.2,
+            }}
+          />
+        )}
+      </MapContainer>
+    </div>
+  );
+};
 
-export default App
+export default App;
